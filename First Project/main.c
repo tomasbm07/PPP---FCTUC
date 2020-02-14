4e52fd65d9d31f2da6abf4406ba411d7446789ed
tomasbm07 <43079732+tomasbm07@users.noreply.github.com>
2020-02-14 16:27:32 +0000

    Updates

--- First Project/main.c
+++ First Project/main.c
@@ -1,8 +1,20 @@
+/* Print Fahrenheit Celsius table for fahr = 0,20, ..., 300 */
 #include <stdio.h>
 
 int main() {
-    printf("Hello, World!\n");
+    int fahr, celsius;
+    int lower, upper, step;
+
+    lower = 0;
+    upper = 300;
+    step = 20;
+
+    fahr = lower;
+    while (fahr <= upper) {
+        celsius = 5 * (fahr - 32) / 9;
+        printf("%3d %6d\n", fahr, celsius);
+        fahr += step;
+    }
+
     return 0;
 }
-
-
